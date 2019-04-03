035e0d84f9797fbefab96c41a0548771c64ec8a5
Alex Burrosquieta <alexburrosquieta@alexs-air.attlocal.net>
2019-04-02 23:18:52 -0500

    Files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Double Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Royal with Cheese", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Chicken Burg", false);
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Timeout
+app.use(timeout(15000));
+app.use(haltOnTimedout);
+
+function haltOnTimedout(req, res, next) {
+   if (!req.timedout) next();
+}
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
